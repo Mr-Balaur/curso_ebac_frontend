94951ca0e53bdd41961d616f5c65c8d022657107
Carlos Batista <carloshenriquealmeidabatista@hotmail.com>
2025-05-19 19:40:22 -0300

    Correção de bugs no retorno da validação.

--- validacao.js
+++ validacao.js
@@ -1,25 +1,31 @@
 const form = document.getElementById('comparador');
 const mensagemSucesso = document.querySelector('.success-message');
 const mensagemErro = document.querySelector('.error-message');
+const valorA = document.getElementById('valorA');
+const valorB = document.getElementById('valorB');
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-const inputA = document.getElementById('valorA');
-const inputB = document.getElementById('valorB');
+const inputA = Number(valorA.value);
+const inputB = Number(valorB.value);
 
-    if (inputB.value > inputA.value){
+    if (inputB > inputA){
      
         mensagemSucesso.style.display = 'block';
+        mensagemErro.style.display = 'none';
 
-        inputA.value = '';
-        inputB.value = '';
+        valorA.value = '';
+        valorB.value = '';
     }
     else{
          document.querySelector('.error-message').style.display = 'block';
+         mensagemSucesso.style.display = 'none';
+
     }
 
     setTimeout(() =>{
         mensagemSucesso.style.display = 'none';
         mensagemErro.style.display = 'none';
-    }, 3 * 1000 );
+    }, 4 * 1000 );
 })
